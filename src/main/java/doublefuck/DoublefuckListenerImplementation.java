65e976e89745235c20b9c895b607c66baaed6b7f
HubertFraczek <hhubcio12@gmail.com>
2020-04-13 15:14:14 +0200

    implementing some doublefuck methods

--- src/main/java/doublefuck/DoublefuckListenerImplementation.java
+++ src/main/java/doublefuck/DoublefuckListenerImplementation.java
@@ -6,21 +6,80 @@ import org.antlr.v4.runtime.ParserRuleContext;
 import org.antlr.v4.runtime.tree.ErrorNode;
 import org.antlr.v4.runtime.tree.TerminalNode;
 
-public class DoublefuckListenerImplementation implements DoublefuckListener {
-    public void enterFile(DoublefuckParser.FileContext ctx) {
+import java.io.BufferedWriter;
+import java.io.IOException;
 
+public class DoublefuckListenerImplementation implements DoublefuckListener {
+    private BufferedWriter writer;
+    private int indents = 0;
+    private int size;
+
+    public DoublefuckListenerImplementation(BufferedWriter bufferedWriter, int size) {
+        this.writer = bufferedWriter;
+        this.size = size;
+    }
+
+    private void putIndents() {
+        try {
+            for (int i=0;i<indents;i++) {
+                writer.write("\t");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void enterFile(DoublefuckParser.FileContext ctx) {
+        try {
+            writer.write("#include <stdio.h>\n \n");
+            writer.write("int main()\n");
+            writer.write("{\n");
+            indents++;
+            putIndents();
+            writer.write("char array["+size+"] = {0};\n");
+            putIndents();
+            writer.write("char *ptr=array;\n");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public void exitFile(DoublefuckParser.FileContext ctx) {
-
+        try {
+            putIndents();
+            writer.write("return 0;\n");
+            indents--;
+            putIndents();
+            writer.write("}\n");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public void enterExpression(DoublefuckParser.ExpressionContext ctx) {
-
+        String txt = ctx.getText();
+        try {
+            if (txt.startsWith("[")) {
+                putIndents();
+                writer.write("while (*ptr) {\n");
+                indents++;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public void exitExpression(DoublefuckParser.ExpressionContext ctx) {
-
+        String txt = ctx.getText();
+        try {
+            if (txt.endsWith("]")) {
+                indents--;
+                putIndents();
+                writer.write("}\n");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public void enterOperator(DoublefuckParser.OperatorContext ctx) {
