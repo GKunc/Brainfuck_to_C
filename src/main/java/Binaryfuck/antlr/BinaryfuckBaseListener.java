10f5b237b60e92609bb842d28294a2d819147a80
HubertFraczek <hhubcio12@gmail.com>
2020-05-05 10:58:33 +0200

    Implement Binaryfuck

---
+++ src/main/java/Binaryfuck/antlr/BinaryfuckBaseListener.java
@@ -0,0 +1,75 @@
+// Generated from Binaryfuck.g4 by ANTLR 4.8
+package Binaryfuck.antlr;
+
+import org.antlr.v4.runtime.ParserRuleContext;
+import org.antlr.v4.runtime.tree.ErrorNode;
+import org.antlr.v4.runtime.tree.TerminalNode;
+
+/**
+ * This class provides an empty implementation of {@link BinaryfuckListener},
+ * which can be extended to create a listener which only needs to handle a subset
+ * of the available methods.
+ */
+public class BinaryfuckBaseListener implements BinaryfuckListener {
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterFile(BinaryfuckParser.FileContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitFile(BinaryfuckParser.FileContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterExpression(BinaryfuckParser.ExpressionContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitExpression(BinaryfuckParser.ExpressionContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterOperator(BinaryfuckParser.OperatorContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitOperator(BinaryfuckParser.OperatorContext ctx) { }
+
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterEveryRule(ParserRuleContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitEveryRule(ParserRuleContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void visitTerminal(TerminalNode node) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void visitErrorNode(ErrorNode node) { }
+}
---
+++ src/main/java/Binaryfuck/antlr/BinaryfuckListener.java
@@ -0,0 +1,41 @@
+// Generated from Binaryfuck.g4 by ANTLR 4.8
+package Binaryfuck.antlr;
+
+import org.antlr.v4.runtime.tree.ParseTreeListener;
+
+/**
+ * This interface defines a complete listener for a parse tree produced by
+ * {@link BinaryfuckParser}.
+ */
+public interface BinaryfuckListener extends ParseTreeListener {
+	/**
+	 * Enter a parse tree produced by {@link BinaryfuckParser#file}.
+	 * @param ctx the parse tree
+	 */
+	void enterFile(BinaryfuckParser.FileContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link BinaryfuckParser#file}.
+	 * @param ctx the parse tree
+	 */
+	void exitFile(BinaryfuckParser.FileContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link BinaryfuckParser#expression}.
+	 * @param ctx the parse tree
+	 */
+	void enterExpression(BinaryfuckParser.ExpressionContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link BinaryfuckParser#expression}.
+	 * @param ctx the parse tree
+	 */
+	void exitExpression(BinaryfuckParser.ExpressionContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link BinaryfuckParser#operator}.
+	 * @param ctx the parse tree
+	 */
+	void enterOperator(BinaryfuckParser.OperatorContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link BinaryfuckParser#operator}.
+	 * @param ctx the parse tree
+	 */
+	void exitOperator(BinaryfuckParser.OperatorContext ctx);
+}
